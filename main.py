2e1bbb9c33744d30c97c42f99fd32536aba18a94
ctiller15 <christiller2015@gmail.com>
2025-01-20 00:05:45 -0500

    initialize project, create logic for adding and removing folders

---
+++ main.py
@@ -0,0 +1,9 @@
+from window import Window
+
+
+def main():
+    print("hello tkinter!")
+    window = Window(800, 600)
+
+if __name__ == "__main__":
+    main()
---
+++ window.py
@@ -0,0 +1,52 @@
+import tkinter
+from tkinter import E, N, S, W, ttk, filedialog
+
+class Window():
+    def __init__(self, width, height):
+        self.__root = tkinter.Tk()
+        # self.__root.wait_visibility(self.__root)
+        self.__root.title = "Quick Poses"
+
+        self.__main_frame = ttk.Frame(self.__root, padding="3 3 12 12", width=self.__root.winfo_screenwidth() - 100, height=self.__root.winfo_screenheight() - 100)
+        self.__main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        self.__selected_folders = []
+
+        self.__build_main_page()
+
+        self.__root.mainloop()
+    
+    def __build_main_page(self):
+
+        ttk.Button(self.__main_frame, text="Add directory", command=self.ask_folder).grid(column=3, row=1, sticky=(W, E))
+
+    def create_delete_func(self, i):
+        def delete_selected_folder():
+            nonlocal i
+            del self.__selected_folders[i]
+
+            # remove all widgets past a certain row
+            for widget in self.__root.grid_slaves():
+                print(widget.grid_info()["row"])
+                if int(widget.grid_info()["row"]) > 1:
+                    widget.grid_forget()
+
+            # Then recreate all of the appropriate widgets.
+            for ind in range(len(self.__selected_folders)):
+                self.__create_folder_entry(self.__selected_folders[ind], ind + 1)
+
+        return delete_selected_folder
+
+    def ask_folder(self):
+        folder_name = filedialog.askdirectory()
+        if len(folder_name) > 0:
+            self.__selected_folders.append(folder_name)
+            self.__create_folder_entry(folder_name, len(self.__selected_folders))
+
+    def __create_folder_entry(self, folder_name, i):
+            new_entry = ttk.Label(self.__root, width=7, text=folder_name, wraplength=100)
+            new_entry.grid(column=0, row=i + 1, sticky="WE")
+
+            # i - 2 to fix 0-based indexing vs starting at row 2.
+            entry_button = ttk.Button(self.__root, width=7, text="de-select", command=self.create_delete_func(i - 1))
+            entry_button.grid(column=2, row=i + 1, sticky="WE")
